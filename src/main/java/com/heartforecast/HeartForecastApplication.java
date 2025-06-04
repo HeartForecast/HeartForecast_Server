ddda74e549d80de6bb6f60a6c60f0e9768b2bb22
GSB0203 <gimseungbin0203@gmail.com>
2025-06-05 00:19:32 +0900

    feat(init): 기본 설정

---
+++ src/main/java/com/heartforecast/HeartForecastApplication.java
@@ -0,0 +1,13 @@
+package com.heartforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartForecastApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HeartForecastApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/heartforecast/HeartForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heartforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartForecastApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
