c9b89871bc4e2c8a4caf7c45aaf96b64ddcc616a
GSB0203 <gimseungbin0203@gmail.com>
2025-06-11 23:05:50 +0900

    feat(User): HeartForecastApplication
    createdAt를 사용하기위해 추가하였습니다.

--- src/main/java/com/heartforecast/HeartForecastApplication.java
+++ src/main/java/com/heartforecast/HeartForecastApplication.java
@@ -2,7 +2,9 @@ package com.heartforecast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class HeartForecastApplication {
 
@@ -11,3 +13,4 @@ public class HeartForecastApplication {
   }
 
 }
+
