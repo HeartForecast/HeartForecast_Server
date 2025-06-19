dd60156fa01a28e6939b700c25fc3a4d921341cd
GSB0203 <gimseungbin0203@gmail.com>
2025-06-20 01:19:08 +0900

    feat(Auth): InvalidCredentialsException
    잘못된 이메일 또는 비밀번호인 경우의 예외 처리를 구현하였습니다.

---
+++ src/main/java/com/heartforecast/domain/auth/exception/InvalidCredentialsException.java
@@ -0,0 +1,11 @@
+package com.heartforecast.domain.auth.exception;
+
+import com.heartforecast.common.exception.security.HeartForecastSecurityException;
+import org.springframework.http.HttpStatus;
+
+public class InvalidCredentialsException extends HeartForecastSecurityException {
+
+    public InvalidCredentialsException() {
+        super(HttpStatus.UNAUTHORIZED, "INVALID_CREDENTIALS", "잘못된 이메일 또는 비밀번호입니다.");
+    }
+}
