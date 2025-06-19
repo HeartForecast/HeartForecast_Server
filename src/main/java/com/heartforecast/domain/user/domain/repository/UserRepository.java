58144af821c46182d0e3c232191d62f25f5b45d0
GSB0203 <gimseungbin0203@gmail.com>
2025-06-20 01:06:20 +0900

    feat(User): UserRepository
    Oauth 로그인 및 자체 로그인을 위해 추가하였습니다.

--- src/main/java/com/heartforecast/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/heartforecast/domain/user/domain/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.heartforecast.domain.user.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
+  Boolean existsByEmail(String email);
+  Users findByEmail(String email);
 }
