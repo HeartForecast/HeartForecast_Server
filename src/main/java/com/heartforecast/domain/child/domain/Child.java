3a319b332ad401eee85d16f8f4a511d957de39db
GSB0203 <gimseungbin0203@gmail.com>
2025-06-20 04:25:09 +0900

    feat(Child): Child
    아이 클래스 구현하였습니다.

---
+++ src/main/java/com/heartforecast/domain/child/domain/Child.java
@@ -0,0 +1,49 @@
+package com.heartforecast.domain.child.domain;
+
+import com.heartforecast.domain.child.domain.value.Gender;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Child {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String username;
+
+  private LocalDate birthdate;
+
+  @Enumerated(EnumType.STRING)
+  private Gender gender;
+
+  private String healthInfo;
+
+  private Integer point;
+
+  @Column(unique = true)
+  private String inviteCode;
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+
+  @Builder
+  public Child(String username, LocalDate birthdate, Gender gender, String healthInfo, String inviteCode) {
+    this.username = username;
+    this.birthdate = birthdate;
+    this.gender = gender;
+    this.healthInfo = healthInfo;
+    this.point = 0;
+    this.inviteCode = inviteCode;
+  }
+}
