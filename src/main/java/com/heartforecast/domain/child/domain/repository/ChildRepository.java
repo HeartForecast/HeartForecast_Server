a22f256e2429bf4a4388aa4967d5b604370b4d06
GSB0203 <gimseungbin0203@gmail.com>
2025-06-20 04:27:02 +0900

    feat(Child): ChildRepository
    아이 클래스를 위해 구현하였습니다.

--- src/main/java/com/heartforecast/domain/child/domain/repository/ChildRepository.java
+++ src/main/java/com/heartforecast/domain/child/domain/repository/ChildRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ChildRepository extends JpaRepository<Child, Long> {
+  boolean existsByInviteCode(String inviteCode);
   Optional<Child> findByInviteCode(String inviteCode);
 }
