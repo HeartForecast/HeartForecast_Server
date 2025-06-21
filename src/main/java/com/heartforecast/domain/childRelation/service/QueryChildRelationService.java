8f3c2fecb9759408411c06bcdfba64fe92f981a9
GSB0203 <gimseungbin0203@gmail.com>
2025-06-21 15:35:50 +0900

    feat(Child): QueryChildRelationService
    아이 조회를 위해 구현하였습니다.

---
+++ src/main/java/com/heartforecast/domain/childRelation/service/QueryChildRelationService.java
@@ -0,0 +1,34 @@
+package com.heartforecast.domain.childRelation.service;
+
+import com.heartforecast.domain.child.domain.Child;
+import com.heartforecast.domain.child.service.QueryChildService;
+import com.heartforecast.domain.childRelation.domain.ChildRelation;
+import com.heartforecast.domain.childRelation.service.implementation.ChildRelationReader;
+import com.heartforecast.domain.user.domain.Users;
+import com.heartforecast.domain.user.service.QueryUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class QueryChildRelationService {
+
+  private final ChildRelationReader childRelationReader;
+  private final QueryChildService queryChildService;
+  private final QueryUserService queryUserService;
+
+  public ChildRelation readOne(Long childId, Long userId) {
+    Child child = queryChildService.readOne(childId);
+    Users user = queryUserService.readOne(userId);
+    return childRelationReader.findByUserAndChild(child, user);
+  }
+
+  public List<ChildRelation> readAll(Long userId) {
+    Users user = queryUserService.readOne(userId);
+    return childRelationReader.findByUser(user);
+  }
+}
