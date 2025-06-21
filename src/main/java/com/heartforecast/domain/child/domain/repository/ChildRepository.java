bd47a26845cab223b3e0ed935d92ca5b5fe09dc4
GSB0203 <gimseungbin0203@gmail.com>
2025-06-21 15:39:18 +0900

    feat(Child): ChildRepository
    Repository 어노테이션을 추가하였습니다.

--- src/main/java/com/heartforecast/domain/child/domain/repository/ChildRepository.java
+++ src/main/java/com/heartforecast/domain/child/domain/repository/ChildRepository.java
@@ -2,9 +2,11 @@ package com.heartforecast.domain.child.domain.repository;
 
 import com.heartforecast.domain.child.domain.Child;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ChildRepository extends JpaRepository<Child, Long> {
   boolean existsByInviteCode(String inviteCode);
   Optional<Child> findByInviteCode(String inviteCode);
