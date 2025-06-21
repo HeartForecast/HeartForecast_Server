fd1d69893ce248b05be6680f7569ac1d308e3478
GSB0203 <gimseungbin0203@gmail.com>
2025-06-21 17:53:47 +0900

    style(ChildRelation): ChildRelationNotFoundException
    Not이 빼져 있던 문제를 해결했습니다.

--- src/main/java/com/heartforecast/domain/childRelation/exception/ChildRelationNotFoundException.java
+++ src/main/java/com/heartforecast/domain/childRelation/exception/ChildRelationNotFoundException.java
@@ -3,8 +3,8 @@ package com.heartforecast.domain.childRelation.exception;
 import com.heartforecast.common.exception.HeartForecastException;
 import org.springframework.http.HttpStatus;
 
-public class ChildRelationFoundException extends HeartForecastException {
-  public ChildRelationFoundException() {
+public class ChildRelationNotFoundException extends HeartForecastException {
+  public ChildRelationNotFoundException() {
     super(HttpStatus.NOT_FOUND, "CHILD_RELATION_NOT_FOUND","아이와의 관계를 찾을 수 없습니다.");
   }
 }
