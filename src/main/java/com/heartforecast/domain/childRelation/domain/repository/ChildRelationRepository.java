059cb7865d3bab8f8a48e0a14baacc84e3dbdb9a
GSB0203 <gimseungbin0203@gmail.com>
2025-06-21 15:37:25 +0900

    feat(Child): ChildRelationRepository
    아이 조회를 위해 구현하였습니다.

--- src/main/java/com/heartforecast/domain/childRelation/domain/repository/ChildRelationRepository.java
+++ src/main/java/com/heartforecast/domain/childRelation/domain/repository/ChildRelationRepository.java
@@ -4,7 +4,14 @@ import com.heartforecast.domain.child.domain.Child;
 import com.heartforecast.domain.childRelation.domain.ChildRelation;
 import com.heartforecast.domain.user.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface ChildRelationRepository extends JpaRepository<ChildRelation, Long> {
   boolean existsByUserAndChild(Users user, Child child);
+  Optional<ChildRelation> findByUserAndChild(Users user, Child child);
+  List<ChildRelation> findByUser(Users user);
 }
