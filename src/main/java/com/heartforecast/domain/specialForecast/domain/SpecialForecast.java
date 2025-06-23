774489ea576d9598d8460f0f6646b4eb5cdefcf1
GSB0203 <gimseungbin0203@gmail.com>
2025-06-23 15:07:39 +0900

    feat(SpecialForecast): SpecialForecast
    특보를 구현하였습니다.

---
+++ src/main/java/com/heartforecast/domain/specialForecast/domain/SpecialForecast.java
@@ -0,0 +1,52 @@
+package com.heartforecast.domain.specialForecast.domain;
+
+import com.heartforecast.domain.child.domain.Child;
+import com.heartforecast.domain.emotionType.domain.EmotionType;
+import com.heartforecast.domain.event.domain.Event;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class SpecialForecast {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  private Event event;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Child child;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  private EmotionType emotionType;
+
+  private String memo;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @Builder
+  public SpecialForecast(Event event, Child child, EmotionType emotionType, String memo) {
+    this.event = event;
+    this.child = child;
+    this.emotionType = emotionType;
+    this.memo = memo;
+  }
+
+  public void update(EmotionType emotionType, String memo) {
+    this.emotionType = emotionType;
+    this.memo = memo;
+  }
+}
