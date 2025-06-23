d1642e078e0e8b25291c16ebc4fcfb713f6f72af
GSB0203 <gimseungbin0203@gmail.com>
2025-06-24 00:38:07 +0900

    style(SpecialForecastRecord): SpecialForecastRecordAlreadyExistsException
    형식을 일치시켰습니다.

--- src/main/java/com/heartforecast/domain/SpecialForecastRecord/exception/SpecialForecastRecordAlreadyExistsException.java
+++ src/main/java/com/heartforecast/domain/SpecialForecastRecord/exception/SpecialForecastRecordAlreadyExistsException.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 
 public class SpecialForecastRecordAlreadyExistsException extends HeartForecastException {
   public SpecialForecastRecordAlreadyExistsException() {
-    super(HttpStatus.CONFLICT, "SPECIAL_FORECAST_RECORD_ALREADY_EXISTS", "특보 기록이 이미 존재합니다.");
+    super(HttpStatus.CONFLICT, "SPECIAL_FORECAST_RECORD_ALREADY_EXISTS", "해당 특보로 생성된 특보 기록이 이미 존재합니다.");
   }
 }
-
