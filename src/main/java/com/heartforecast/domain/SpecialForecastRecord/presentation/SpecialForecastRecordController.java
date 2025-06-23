a0cfb52af65c5bad93d953e4535152b5f4ff7fde
GSB0203 <gimseungbin0203@gmail.com>
2025-06-23 22:18:56 +0900

    feat(SpecialForecastRecord): SpecialForecastRecordController
    특보 기록 조회를 위해 구현하였습니다.

--- src/main/java/com/heartforecast/domain/SpecialForecastRecord/presentation/SpecialForecastRecordController.java
+++ src/main/java/com/heartforecast/domain/SpecialForecastRecord/presentation/SpecialForecastRecordController.java
@@ -1,12 +1,13 @@
 package com.heartforecast.domain.SpecialForecastRecord.presentation;
 
 import com.heartforecast.domain.SpecialForecastRecord.presentation.dto.request.SpecialForecastRecordCreateRequest;
+import com.heartforecast.domain.SpecialForecastRecord.presentation.dto.response.SpecialForecastRecordResponse;
 import com.heartforecast.domain.SpecialForecastRecord.service.CommandSpecialForecastRecordService;
+import com.heartforecast.domain.SpecialForecastRecord.service.QuerySpecialForecastRecordService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,9 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpecialForecastRecordController {
 
   private final CommandSpecialForecastRecordService commandSpecialForecastRecordService;
+  private final QuerySpecialForecastRecordService querySpecialForecastRecordService;
 
   @PostMapping("specialForecastRecord")
   public void createSpecialForecastRecord(@RequestBody SpecialForecastRecordCreateRequest request) {
     commandSpecialForecastRecordService.create(request);
+  }
+
+  @GetMapping("/{child-id}/{specialForecastRecord-id}")
+  public SpecialForecastRecordResponse getSpecialForecastRecord(
+      @PathVariable("child-id") Long childId,
+      @PathVariable("specialForecastRecord-id") Long specialForecastRecordId) {
+    return SpecialForecastRecordResponse.from(querySpecialForecastRecordService.readOne(specialForecastRecordId, childId));
+  }
+
+  @GetMapping("/{child-id}")
+  public List<SpecialForecastRecordResponse> getSpecialForecastRecords(@PathVariable("child-id") Long childId) {
+    return querySpecialForecastRecordService.readAll(childId).stream()
+        .map(SpecialForecastRecordResponse::from)
+        .toList();
   }
 }
