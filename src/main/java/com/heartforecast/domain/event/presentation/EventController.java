507330c1df05de6e4b43328e9f7a56137a419ad0
GSB0203 <gimseungbin0203@gmail.com>
2025-06-23 15:02:22 +0900

    feat(Event): EventController
    아이 조회를 추가하면서 용도에 맞게 이름을 재설정하였습니다.

--- src/main/java/com/heartforecast/domain/event/presentation/EventController.java
+++ src/main/java/com/heartforecast/domain/event/presentation/EventController.java
@@ -30,8 +30,9 @@ public class EventController {
   public EventResponse getEvent(
       @PathVariable("child-id") Long childId,
       @PathVariable("event-id") Long eventId) {
-    Event event = queryEventService.readOne(eventId, getMemberId());
+    Event event = queryEventService.findOneByUser(eventId, getMemberId());
     return EventResponse.from(
+        event.getId(),
         childId,
         event.getDate(),
         event.getTitle(),
@@ -44,6 +45,7 @@ public class EventController {
     List<Event> events = queryEventService.readAll(childId, getMemberId());
     return events.stream()
         .map(event -> EventResponse.from(
+            event.getId(),
             event.getChild().getId(),
             event.getDate(),
             event.getTitle(),
