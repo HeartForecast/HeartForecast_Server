ace5a1624812fdbff4f5468cdae48fc16ad7c2f9
GSB0203 <gimseungbin0203@gmail.com>
2025-06-23 17:35:14 +0900

    feat(SpecialForecast): SpecialForecastDeleter
    특보 삭제을 위해 구현하였습니다.

---
+++ src/main/java/com/heartforecast/domain/specialForecast/service/implementation/SpecialForecastDeleter.java
@@ -0,0 +1,17 @@
+package com.heartforecast.domain.specialForecast.service.implementation;
+
+import com.heartforecast.domain.specialForecast.domain.SpecialForecast;
+import com.heartforecast.domain.specialForecast.domain.repository.SpecialForecastRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SpecialForecastDeleter {
+
+  private final SpecialForecastRepository specialForecastRepository;
+
+  public void delete(SpecialForecast specialForecast) {
+    specialForecastRepository.delete(specialForecast);
+  }
+}
