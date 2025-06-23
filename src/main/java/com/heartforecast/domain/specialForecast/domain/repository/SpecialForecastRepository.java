36fe99ce77311ea0e6da1c26fe7dc1f09383c855
GSB0203 <gimseungbin0203@gmail.com>
2025-06-23 15:50:39 +0900

    feat(SpecialForecast): SpecialForecastRepository
    특보 조회를 위해 구현하였습니다.

--- src/main/java/com/heartforecast/domain/specialForecast/domain/repository/SpecialForecastRepository.java
+++ src/main/java/com/heartforecast/domain/specialForecast/domain/repository/SpecialForecastRepository.java
@@ -1,11 +1,15 @@
 package com.heartforecast.domain.specialForecast.domain.repository;
 
+import com.heartforecast.domain.child.domain.Child;
 import com.heartforecast.domain.event.domain.Event;
 import com.heartforecast.domain.specialForecast.domain.SpecialForecast;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SpecialForecastRepository extends JpaRepository<SpecialForecast, Long> {
   boolean existsByEvent(Event event);
+  Optional<SpecialForecast> findByIdAndChild(Long id, Child child);
 }
