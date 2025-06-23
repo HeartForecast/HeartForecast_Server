b1f7f79c446873b6f16b466a13b9234a0b30bc15
GSB0203 <gimseungbin0203@gmail.com>
2025-06-23 15:46:49 +0900

    fix(QueryEmotionType): QueryEmotionTypeService
    잘못되어 있던 Transactional를 수정하였습니다.

--- src/main/java/com/heartforecast/domain/emotionType/service/QueryEmotionTypeService.java
+++ src/main/java/com/heartforecast/domain/emotionType/service/QueryEmotionTypeService.java
@@ -2,9 +2,9 @@ package com.heartforecast.domain.emotionType.service;
 
 import com.heartforecast.domain.emotionType.domain.EmotionType;
 import com.heartforecast.domain.emotionType.service.implementation.EmotionTypeReader;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
