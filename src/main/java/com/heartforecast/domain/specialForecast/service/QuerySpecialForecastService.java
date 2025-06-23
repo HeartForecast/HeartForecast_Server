b52f0728c8da913867c7a3add86246de05ec14fd
GSB0203 <gimseungbin0203@gmail.com>
2025-06-24 00:31:34 +0900

    feat(SpecialForecast): QuerySpecialForecastService
    이벤트 삭제시 특보가 삭제되도록 구현하기 위해 추가하였습니다.

--- src/main/java/com/heartforecast/domain/specialForecast/service/QuerySpecialForecastService.java
+++ src/main/java/com/heartforecast/domain/specialForecast/service/QuerySpecialForecastService.java
@@ -2,6 +2,7 @@ package com.heartforecast.domain.specialForecast.service;
 
 import com.heartforecast.domain.child.domain.Child;
 import com.heartforecast.domain.child.service.QueryChildService;
+import com.heartforecast.domain.event.service.QueryEventService;
 import com.heartforecast.domain.specialForecast.domain.SpecialForecast;
 import com.heartforecast.domain.specialForecast.service.implementation.SpecialForecastReader;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,7 @@ public class QuerySpecialForecastService {
 
   private final SpecialForecastReader specialForecastReader;
   private final QueryChildService queryChildService;
+  private final QueryEventService queryEventService;
 
   public SpecialForecast readOne(Long specialForecastId, Long childId) {
     Child child = queryChildService.readOne(childId);
@@ -28,5 +30,9 @@ public class QuerySpecialForecastService {
     Child child = queryChildService.readOne(childId);
 
     return specialForecastReader.findAllByChild(child);
+  }
+
+  public SpecialForecast findByEvent(Long eventId, Long userId) {
+    return specialForecastReader.findByEvent(queryEventService.findOneByUser(eventId, userId));
   }
 }
