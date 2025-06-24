5d57ec16929e8bcaf05b6959b5dc1c3e38909d21
GSB0203 <gimseungbin0203@gmail.com>
2025-06-24 20:27:18 +0900

    feat(Forecast): ForecastRepository
    예보 조회를 위해 추가하였습니다.

--- src/main/java/com/heartforecast/domain/forecast/domain/repository/ForecastRepository.java
+++ src/main/java/com/heartforecast/domain/forecast/domain/repository/ForecastRepository.java
@@ -1,7 +1,11 @@
 package com.heartforecast.domain.forecast.domain.repository;
 
+import com.heartforecast.domain.child.domain.Child;
 import com.heartforecast.domain.forecast.domain.Forecast;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ForecastRepository extends JpaRepository<Forecast, Long> {
+  Optional<Forecast> findByIdAndChild(Long id, Child child);
 }
