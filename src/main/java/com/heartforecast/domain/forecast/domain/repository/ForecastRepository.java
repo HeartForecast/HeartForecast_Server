93ee268b6e52196556170548c894478a842ad156
GSB0203 <gimseungbin0203@gmail.com>
2025-06-24 20:51:59 +0900

    feat(Forecast): ForecastRepository
    예보 생성시 이미 해당 날짜, 시간대 예보가 있을 경우의 예외를 구현하였습니다.

--- src/main/java/com/heartforecast/domain/forecast/domain/repository/ForecastRepository.java
+++ src/main/java/com/heartforecast/domain/forecast/domain/repository/ForecastRepository.java
@@ -2,12 +2,15 @@ package com.heartforecast.domain.forecast.domain.repository;
 
 import com.heartforecast.domain.child.domain.Child;
 import com.heartforecast.domain.forecast.domain.Forecast;
+import com.heartforecast.domain.forecast.domain.value.TimeZone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface ForecastRepository extends JpaRepository<Forecast, Long> {
   Optional<Forecast> findByIdAndChild(Long id, Child child);
   List<Forecast> findByChild(Child child);
+  boolean existsByChildAndDateAndTimeZone(Child child, LocalDate date, TimeZone timeZone);
 }
