1f0c975d50d747046c2a5c3ba6f51f86e6f23914
GSB0203 <gimseungbin0203@gmail.com>
2025-06-25 01:33:32 +0900

    feat(ForecastRecord): ForecastRecordController
    예보 기록 조회를 구현하였습니다.

--- src/main/java/com/heartforecast/domain/forecastRecord/presentation/ForecastRecordController.java
+++ src/main/java/com/heartforecast/domain/forecastRecord/presentation/ForecastRecordController.java
@@ -1,9 +1,14 @@
 package com.heartforecast.domain.forecastRecord.presentation;
 
 import com.heartforecast.domain.forecastRecord.presentation.dto.request.ForecastRecordCreateRequest;
+import com.heartforecast.domain.forecastRecord.presentation.dto.response.ForecastRecordResponse;
 import com.heartforecast.domain.forecastRecord.service.CommandForecastRecordService;
+import com.heartforecast.domain.forecastRecord.service.QueryForecastRecordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,9 +16,24 @@ import org.springframework.web.bind.annotation.*;
 public class ForecastRecordController {
 
   private final CommandForecastRecordService commandForecastRecordService;
+  private final QueryForecastRecordService queryForecastRecordService;
 
   @PostMapping("/forecastRecord")
   public void createForecastRecord(@RequestBody ForecastRecordCreateRequest request) {
     commandForecastRecordService.create(request);
+  }
+
+  @GetMapping("/{child-id}/{date}")
+  public List<ForecastRecordResponse> getForecastRecord(@PathVariable("child-id") Long childId, @PathVariable LocalDate date) {
+    return queryForecastRecordService.findDate(date, childId).stream()
+        .map(ForecastRecordResponse::from)
+        .toList();
+  }
+
+  @GetMapping("/{child-id}")
+  public List<ForecastRecordResponse> getForecastRecords(@PathVariable("child-id") Long childId) {
+    return queryForecastRecordService.readAll(childId).stream()
+        .map(ForecastRecordResponse::from)
+        .toList();
   }
 }
