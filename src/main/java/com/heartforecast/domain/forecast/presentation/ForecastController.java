2e4cba7fe39960f0ddbe08f58fb92c7bdfdc875b
GSB0203 <gimseungbin0203@gmail.com>
2025-06-24 20:28:31 +0900

    feat(Forecast): ForecastController
    예보 조회를 위해 추가하였습니다.

--- src/main/java/com/heartforecast/domain/forecast/presentation/ForecastController.java
+++ src/main/java/com/heartforecast/domain/forecast/presentation/ForecastController.java
@@ -1,12 +1,11 @@
 package com.heartforecast.domain.forecast.presentation;
 
 import com.heartforecast.domain.forecast.presentation.dto.request.ForecastCreateRequest;
+import com.heartforecast.domain.forecast.presentation.dto.response.ForecastResponse;
 import com.heartforecast.domain.forecast.service.CommandForecastService;
+import com.heartforecast.domain.forecast.service.QueryForecastService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,9 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ForecastController {
 
   private final CommandForecastService commandForecastService;
+  private final QueryForecastService queryForecastService;
 
   @PostMapping("/forecast")
   public void createForecast(@RequestBody ForecastCreateRequest request) {
     commandForecastService.create(request);
+  }
+
+  @GetMapping("/{child-id}/{forecast-id}")
+  public ForecastResponse getForecast(@PathVariable("child-id") Long childId, @PathVariable("forecast-id") Long forecastId) {
+    return ForecastResponse.from(queryForecastService.readOne(forecastId, childId));
   }
 }
