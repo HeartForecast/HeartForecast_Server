79321e3f019a0e085580b28036016801aa4decb1
GSB0203 <gimseungbin0203@gmail.com>
2025-06-24 21:44:22 +0900

    feat(Forecast): ForecastRepository
    어노테이션을 추가했습니다.

--- src/main/java/com/heartforecast/domain/forecast/domain/repository/ForecastRepository.java
+++ src/main/java/com/heartforecast/domain/forecast/domain/repository/ForecastRepository.java
@@ -4,11 +4,13 @@ import com.heartforecast.domain.child.domain.Child;
 import com.heartforecast.domain.forecast.domain.Forecast;
 import com.heartforecast.domain.forecast.domain.value.TimeZone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ForecastRepository extends JpaRepository<Forecast, Long> {
   Optional<Forecast> findByIdAndChild(Long id, Child child);
   List<Forecast> findByChild(Child child);
