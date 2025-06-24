390d244388fc3ac22c0713e5d40ec46345ccda80
GSB0203 <gimseungbin0203@gmail.com>
2025-06-24 19:53:47 +0900

    feat(Forecast): CommandForecastService
    예보 생성을 위해 구현하였습니다.

---
+++ src/main/java/com/heartforecast/domain/forecast/service/CommandForecastService.java
@@ -0,0 +1,36 @@
+package com.heartforecast.domain.forecast.service;
+
+import com.heartforecast.domain.child.domain.Child;
+import com.heartforecast.domain.child.service.QueryChildService;
+import com.heartforecast.domain.emotionType.domain.EmotionType;
+import com.heartforecast.domain.emotionType.service.QueryEmotionTypeService;
+import com.heartforecast.domain.forecast.domain.Forecast;
+import com.heartforecast.domain.forecast.presentation.dto.request.ForecastCreateRequest;
+import com.heartforecast.domain.forecast.service.implementation.ForecastCreator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CommandForecastService {
+
+  private final ForecastCreator forecastCreator;
+  private final QueryChildService queryChildService;
+  private final QueryEmotionTypeService queryEmotionTypeService;
+
+  public void create(ForecastCreateRequest request) {
+    Child child = queryChildService.readOne(request.childId());
+    EmotionType emotionType = queryEmotionTypeService.readOne(request.emotionTypeId());
+
+    Forecast forecast = Forecast.builder()
+        .child(child)
+        .emotionType(emotionType)
+        .date(request.date())
+        .timeZone(request.timeZone())
+        .memo(request.memo())
+        .build();
+    forecastCreator.create(forecast);
+  }
+}
