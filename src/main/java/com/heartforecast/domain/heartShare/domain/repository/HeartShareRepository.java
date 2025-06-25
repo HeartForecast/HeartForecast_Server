38ff5f63ff4d65e5d3760775db99805b3c746799
GSB0203 <gimseungbin0203@gmail.com>
2025-06-25 13:44:21 +0900

    remove(HeartShare): HeartShareRepository
    마음공유 접근 권한 확인 로직을 분리하면서 필요 없어져 제거하였습니다.

--- src/main/java/com/heartforecast/domain/heartShare/domain/repository/HeartShareRepository.java
+++ src/main/java/com/heartforecast/domain/heartShare/domain/repository/HeartShareRepository.java
@@ -6,10 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface HeartShareRepository extends JpaRepository<HeartShare, Long> {
-  Optional<HeartShare> findByIdAndUser(Long id, Users user);
   List<HeartShare> findAllByUser(Users user);
 }
