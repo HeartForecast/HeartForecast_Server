4eb172648bba5090a523f1c0a327297c5f36e8c1
GSB0203 <gimseungbin0203@gmail.com>
2025-06-25 13:42:23 +0900

    feat(HeartShare): HeartShareAccessDeniedException
    마음공유 접근 권한이 없는 경우의 예외를 구현하였습니다.

---
+++ src/main/java/com/heartforecast/domain/heartShare/exception/HeartShareAccessDeniedException.java
@@ -0,0 +1,10 @@
+package com.heartforecast.domain.heartShare.exception;
+
+import com.heartforecast.common.exception.HeartForecastException;
+import org.springframework.http.HttpStatus;
+
+public class HeartShareAccessDeniedException extends HeartForecastException {
+  public HeartShareAccessDeniedException() {
+    super(HttpStatus.FORBIDDEN, "HEART_SHARE_ACCESS_DENIED", "해당 마음공유에 접근 권한이 없습니다.");
+  }
+}
