2c3c7893fe9a0746ffc481ed4d5c31a47fae0426
GSB0203 <gimseungbin0203@gmail.com>
2025-06-26 00:21:03 +0900

    feat(ChildRelation): ChildRelationResponse
    스웨거를 추가하였습니다.

--- src/main/java/com/heartforecast/domain/childRelation/presentation/dto/response/ChildRelationResponse.java
+++ src/main/java/com/heartforecast/domain/childRelation/presentation/dto/response/ChildRelationResponse.java
@@ -2,8 +2,13 @@ package com.heartforecast.domain.childRelation.presentation.dto.response;
 
 import com.heartforecast.domain.child.presentation.dto.response.ChildResponse;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "돌봄관계 반환 DTO")
 public record ChildRelationResponse(
+    @Schema(description = "아이 정보")
     ChildResponse childResponse,
+    @Schema(description = "관계 유형 (예: 부모, 보호자)", example = "엄마")
     String role
 ) {
   public static ChildRelationResponse of(ChildResponse childResponse, String role) {
