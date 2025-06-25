c99d187b1850191fb55e386a9c8f31d146c18c4f
GSB0203 <gimseungbin0203@gmail.com>
2025-06-25 10:34:25 +0900

    feat(Forecast): SpecialForecastCreateRequest
    날짜를 추가하였습니다.

--- src/main/java/com/heartforecast/domain/specialForecast/presentation/dto/request/SpecialForecastCreateRequest.java
+++ src/main/java/com/heartforecast/domain/specialForecast/presentation/dto/request/SpecialForecastCreateRequest.java
@@ -1,9 +1,12 @@
 package com.heartforecast.domain.specialForecast.presentation.dto.request;
 
 
+import java.time.LocalDate;
+
 public record SpecialForecastCreateRequest(
     Long eventId,
     Long childId,
     Long emotionTypeId,
+    LocalDate date,
     String memo
 ) {}
