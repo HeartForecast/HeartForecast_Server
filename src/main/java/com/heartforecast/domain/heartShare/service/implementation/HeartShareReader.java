7a091805a9f4781b404f2106280ee6bd5c65ac3a
GSB0203 <gimseungbin0203@gmail.com>
2025-06-25 12:19:39 +0900

    feat(HeartShare): HeartShareReader
    내가 작성한 마음공유 전체 조회를 위해 구현하였습니다.

--- src/main/java/com/heartforecast/domain/heartShare/service/implementation/HeartShareReader.java
+++ src/main/java/com/heartforecast/domain/heartShare/service/implementation/HeartShareReader.java
@@ -3,6 +3,7 @@ package com.heartforecast.domain.heartShare.service.implementation;
 import com.heartforecast.domain.heartShare.domain.HeartShare;
 import com.heartforecast.domain.heartShare.domain.repository.HeartShareRepository;
 import com.heartforecast.domain.heartShare.exception.HeartShareNotFoundException;
+import com.heartforecast.domain.user.domain.Users;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +22,9 @@ public class HeartShareReader {
 
   public List<HeartShare> findAll() {
     return heartShareRepository.findAll();
+  }
+
+  public List<HeartShare> findAllByUser(Users user) {
+    return heartShareRepository.findAllByUser(user);
   }
 }
