96ee8e7d6fda62d5ebd8a516a1950a4aebe58d1f
GSB0203 <gimseungbin0203@gmail.com>
2025-07-07 15:47:34 +0900

    feat(SpecialForecastRecord): SpecialForecastRecordCreateRequest
    남은 기간 안에 구현하기 위해 기능을 조절하였습니다.

--- src/main/java/com/heartForecast/domain/specialForecastRecord/presentation/dto/request/SpecialForecastRecordCreateRequest.java
+++ src/main/java/com/heartForecast/domain/specialForecastRecord/presentation/dto/request/SpecialForecastRecordCreateRequest.java
@@ -1,8 +1,10 @@
 package com.heartForecast.domain.specialForecastRecord.presentation.dto.request;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 
+@Hidden
 @Schema(description = "특보 기록 생성 요청 DTO")
 public record SpecialForecastRecordCreateRequest(
     @Schema(description = "특보 ID", example = "12")
