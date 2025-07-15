9bd9b10b00da397fc255fed25ca6020048c0d478
GSB0203 <gimseungbin0203@gmail.com>
2025-07-16 03:15:42 +0900

    feat(EmotionType): QueryEmotionTypeService
    감정 전체 조회를 구현하였습니다.

--- src/main/java/com/heartForecast/domain/emotionType/service/QueryEmotionTypeService.java
+++ src/main/java/com/heartForecast/domain/emotionType/service/QueryEmotionTypeService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -15,5 +17,9 @@ public class QueryEmotionTypeService {
 
   public EmotionType readOne(Long id) {
     return emotionTypeReader.findById(id);
+  }
+
+  public List<EmotionType> readAll() {
+    return emotionTypeReader.findAll();
   }
 }
