1871f093e87b51007140da69c2c36b3e3f6dcafb
GSB0203 <gimseungbin0203@gmail.com>
2025-07-16 00:19:07 +0900

    feat(Child): ChildResponse
    아이 관계 등록일을 추가하기 위해 변경하였습니다.

--- src/main/java/com/heartForecast/domain/child/presentation/dto/response/ChildResponse.java
+++ src/main/java/com/heartForecast/domain/child/presentation/dto/response/ChildResponse.java
@@ -4,7 +4,9 @@ import com.heartForecast.domain.child.domain.Child;
 import com.heartForecast.domain.child.domain.value.Gender;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import com.heartForecast.domain.childRelation.domain.ChildRelation;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "아이 정보 반환 DTO")
@@ -19,20 +21,23 @@ public record ChildResponse(
     Gender gender,
     @Schema(description = "아이 건강 정보", example = "알레르기: 땅콩")
     String healthInfo,
+    @Schema(description = "등록일", example = "2021-11-08T11:44:30.327959")
+    LocalDateTime createdAt,
     @Schema(description = "아이 포인트", example = "100")
     Integer point,
     @Schema(description = "아이 고유 초대 코드", example = "ABCD1234")
     String inviteCode
 ) {
-  public static ChildResponse from(Child child) {
+  public static ChildResponse from(ChildRelation childRelation) {
     return new ChildResponse(
-        child.getId(),
-        child.getUsername(),
-        child.getBirthdate(),
-        child.getGender(),
-        child.getHealthInfo(),
-        child.getPoint(),
-        child.getInviteCode()
+        childRelation.getChild().getId(),
+        childRelation.getChild().getUsername(),
+        childRelation.getChild().getBirthdate(),
+        childRelation.getChild().getGender(),
+        childRelation.getChild().getHealthInfo(),
+        childRelation.getCreatedAt(),
+        childRelation.getChild().getPoint(),
+        childRelation.getChild().getInviteCode()
     );
   }
 }
