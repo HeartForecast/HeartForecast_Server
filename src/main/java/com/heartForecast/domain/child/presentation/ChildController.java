6022262311acfacc44edee5e899a79b28fbcf759
GSB0203 <gimseungbin0203@gmail.com>
2025-07-21 15:31:06 +0900

    fix(Child): ChildController
    아이 삭제시 연관관계로 인해 삭제 되지 않던 오류를 해결하였습니다.

--- src/main/java/com/heartForecast/domain/child/presentation/ChildController.java
+++ src/main/java/com/heartForecast/domain/child/presentation/ChildController.java
@@ -4,6 +4,8 @@ import com.heartForecast.domain.child.presentation.dto.request.ChildCreateReques
 import com.heartForecast.domain.child.presentation.dto.request.ChildUpdateRequest;
 import com.heartForecast.domain.child.service.CommandChildService;
 import com.heartForecast.domain.childRelation.service.CommandChildRelationService;
+import com.heartForecast.domain.forecast.service.CommandForecastService;
+import com.heartForecast.domain.forecastRecord.service.CommandForecastRecordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +22,8 @@ public class ChildController {
 
   private final CommandChildService commandChildService;
   private final CommandChildRelationService commandChildRelationService;
+  private final CommandForecastService commandForecastService;
+  private final CommandForecastRecordService commandForecastRecordService;
 
   @Operation(summary = "아이 생성", description = "새로운 아이를 생성하고 현재 유저의 연관관계로 저장합니다.")
   @PostMapping("/child")
@@ -36,6 +40,8 @@ public class ChildController {
   @Operation(summary = "아이 삭제", description = "아이를 삭제하고 연관된 데이터를 모두 제거합니다.")
   @DeleteMapping("/{child-id}")
   public void deleteChild(@PathVariable("child-id") Long childId) {
+    commandForecastRecordService.deleteAll(childId);
+    commandForecastService.deleteAll(childId);
     commandChildRelationService.deleteAll(childId, getMemberId());
     commandChildService.delete(childId);
   }
